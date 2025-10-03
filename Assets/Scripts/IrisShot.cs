0b9cdf0f1dfd337ea414e6af32ed51e6081ae9d5
和田紘季 <2247027@ecc.ac.jp>
2025-10-03 10:56:05 +0900

    プルしてー

--- Assets/Scripts/IrisShot.cs
+++ Assets/Scripts/IrisShot.cs
@@ -1,23 +1,62 @@
 using UnityEngine;
 using DG.Tweening;
+//using Coffee.UIExtensions;
 public class IrisShot : MonoBehaviour
 {
+    public static IrisShot instance;
+
     [SerializeField] RectTransform unmask;
     readonly Vector2 IRIS_IN_SCALE = new Vector2(30, 30);
+    //public Vector3 IRIS_IN_SCALE = Vector3.one;
     readonly float SCALE_DURATION = 1f;
 
-    public void IrisIN()
-    {
-        //unmask.DOScale(IRIS_IN_SCALE, SCALE_DURATION).SetEase(Ease.InCubic);
-    }
     void Start()
     {
-        
+        unmask.localScale = IRIS_IN_SCALE;
     }
 
+    void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
+    }
+    public void IrisIN()
+    {
+
+        unmask.DOScale(new Vector3(0, 0, 0), SCALE_DURATION).SetEase(Ease.OutCubic);
+
+        /*unmask.DOScale(IRIS_IN_SCALE, SCALE_DURATION)
+          .SetEase(Ease.InCubic)
+          .OnComplete(() => {
+              onComplete?.Invoke();
+          });
+        */
+    }
+
+    public void IrisOut()
+    {
+        unmask.DOScale(IRIS_IN_SCALE, SCALE_DURATION).SetEase(Ease.InCubic);
+        
+        /*
+        unmask.DOScale(Vector3.zero, SCALE_DURATION)
+          .SetEase(Ease.OutCubic)
+          .OnComplete(() => {
+              onComplete?.Invoke();
+          });
+        */
+    }
     
+    
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            IrisIN();
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            IrisOut();
+        }
     }
 }
--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -1,15 +1,40 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using DG.Tweening;
 public class SceneTransition : MonoBehaviour
 {
     public string sceneName = "";
-
+    //private bool SceneFlag = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
         if (collision.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene(sceneName);
+            IrisShot.instance.IrisIN();
+            TimerManager.instance.StopTimer();
+            //三秒後にシーン遷移
+            DOVirtual.DelayedCall(2.0f, () =>
+            {
+                SceneManager.LoadScene(sceneName);
+            });
+            //IrisShot.instance.IrisOut();
+            /*IrisShot.instance.IrisIN();
+            SceneFlag = true;
+            Debug.Log("Flagが真になりました！");
+            if (IrisShot.instance != null)
+            {
+                //IrisShot.instance.IrisIN(() => {
+                //    SceneManager.LoadScene(sceneName);
+                //});
+
+            }
+            else
+            {
+                Debug.Log("Flagが null　です。");
+            }
+            */
+
+
         }
     }
 }
