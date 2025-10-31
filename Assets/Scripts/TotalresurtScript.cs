ac55a841d402e43d0b3dea1d318d13fa83139a3a
和田紘季 <2247027@ecc.ac.jp>
2025-10-31 17:15:53 +0900

    プル

--- Assets/Scripts/TotalresurtScript.cs
+++ Assets/Scripts/TotalresurtScript.cs
@@ -6,9 +6,11 @@ public class TotalResultScript : MonoBehaviour
     public GameObject resultPanel;      // 黒幕パネル
     public TextMeshProUGUI resultText;  // 時間表示テキスト
     public TextMeshProUGUI deathText;
+    public TextMeshProUGUI rankText;
 
     public float delayTime = 3f;         // 黒幕が出るまでの待ち時間
     private bool resultShown = false;   // 一度だけ表示するフラグ
+    private string rank;
 
     void Start()
     {
@@ -19,7 +21,6 @@ public class TotalResultScript : MonoBehaviour
         AudioManager.instance.PlayBGM("BGM3");
         AudioManager.instance.PlaySE2("BGM_last");
         ShowResult();
-        // delayTime秒後にリザルトを表示
         //Invoke("ShowResult", delayTime);
     }
 
@@ -28,8 +29,8 @@ public class TotalResultScript : MonoBehaviour
         if (resultShown) return;
         resultShown = true;
 
-            // 黒幕パネルを表示
-            //resultPanel.SetActive(true);
+        // 黒幕パネルを表示
+        //resultPanel.SetActive(true);
 
         // ステージの総経過時間を取得
         float currentTime = TimerManager.instance != null ? TimerManager.instance.GetElapsedTime() : 0f;
@@ -38,14 +39,39 @@ public class TotalResultScript : MonoBehaviour
         resultText.gameObject.SetActive(true);
         resultText.text = "Time: " + FormatTime(currentTime);
         deathText.text = "Death:" + deaths.ToString();
-        //resultText.text = "Time: " + FormatTime(currentTime)+"\n"+ "Deaths:" + deaths.ToString();
-
-        
+        //やるならランク表示
+        rank = GetRank(currentTime);
+        //rankText.text = "RANK:" + rank;
+        ResultRank(rank);
     }
-
-    void ResultRank()
+    string GetRank(float time)
     {
-
+        if (time < 60f) return "S";
+        if (time < 120f) return "A";
+        if (time < 180f) return "B";
+        else return "C";
+    }
+    void ResultRank(string rank)
+    {
+        //画像を入れるやつ
+        switch (rank)
+        {
+            case "S":
+                rankImage.sprite = rankS;
+                break;
+            case "A":
+                rankImage.sprite = rankA;
+                break;
+            case "B":
+                rankImage.sprite = rankB;
+                break;
+            case "C":
+                rankImage.sprite = rankC;
+                break;
+            default:
+                rankImage.sprite = null;
+                break;
+        }
     }
     //public void HideResult()//リザルトを閉じる
     //{
